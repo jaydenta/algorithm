c5e788f2e3e7c18a7f9a4a7cc211197a4243bf94
Jayden Ta <jaydenta8@gmail.com>
2018-08-08 23:03:35 -0700

    add timing function

---
+++ operate_file.py
@@ -0,0 +1,25 @@
+import time
+def compare_add():
+	def add(l):
+		x = 0
+		for i in l:
+			x = x + i 
+		return x
+
+	start = time.time()
+	x = 1000000
+	print('Sum of 0 to {}'.format(x) + ' using add function is {}'.format(add(range(x))))
+	end = time.time()
+	print('add function takes {}'.format(end - start) + ' seconds to finish')
+
+	def builtin_add(l):
+		lSum = sum(l)
+		#print(lSum)
+		return lSum
+
+	start = time.time()
+	x = 1000000
+	print('Sum of 0 to {}'.format(x) + ' using builtin_add is{}'.format(builtin_add(range(x))))
+	end = time.time()
+	print('builtin_add takes {}'.format(end - start) + ' seconds to finish')
+
